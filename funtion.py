df6893b443d63911462fc859f710096e11b434f1
SI-Tanbir <si017490@gmail.com>
2025-03-17 17:09:34 +0600

    learn user defined function

---
+++ funtion.py
@@ -0,0 +1,26 @@
+
+# 2 type function
+ 
+
+#  1 - user define function
+# 2 - built in function 
+
+def my_first_function():
+    a=10
+    b=10
+    print(a+b)
+
+
+def add_two_number(a,b): #arguments
+    
+    print(a+b)
+
+
+def multiply_number(a,b):
+    print(a*b)
+
+
+
+multiply_number(2,2)
+# add_two_number(5,20) #parameters
+# my_first_function()
