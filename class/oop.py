2da3ed4ac02ba8e482985fc8538c83eead536cc6
SI-Tanbir <si017490@gmail.com>
2025-03-18 23:01:31 +0600

    make my first class

---
+++ class/oop.py
@@ -0,0 +1,24 @@
+#car class making
+
+class Car: 
+    def __init__(self):
+        self.brand=''
+        self.model=''
+
+
+car1=Car()
+car1.brand='Toyota'
+car1.model='Corolla'
+
+print(car1.brand)
+print(car1.model)
+
+car2=Car()
+car2.brand='Ford'
+car2.model='Mustang'
+
+print(car2.brand)
+print(car2.model)
+   
+
+
